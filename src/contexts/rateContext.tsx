44471033b1f7b0dee851019cac1c3d311c4b7c41
mudziok <mudziok@gmail.com>
2022-03-14 00:59:45 +0100

    Create RateContext

---
+++ src/contexts/rateContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, FC, useCallback, useEffect, useState } from "react";
+
+interface Ratings {
+    [key: number]: number,
+}
+
+export const RateContext = createContext({
+    ratings: {} as Ratings,
+    recentRatings: {} as Ratings,
+    rateMovie: (movieId: number, rating: number) => {},
+});
+
+export const RateContextProvider:FC = ({children}) => {
+    const [ratings, setRatings] = useState<Ratings>(() => {
+        const localRatingsJSON = localStorage.getItem('ratings') || "";
+        const localRatings = JSON.parse(localRatingsJSON);
+        return localRatings;
+    });
+
+    const [recentRatings, setRecentRatings] = useState<Ratings>({});
+
+    const rateMovie = useCallback((movieId: number, rating: number) => {
+        setRatings(prev => ({...prev, [movieId]: rating}));
+        setRecentRatings(prev => ({...prev, [movieId]: rating}))
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('ratings', JSON.stringify(ratings));   
+    }, [rateMovie, ratings])
+
+    return (
+        <RateContext.Provider value={{ratings, recentRatings, rateMovie}}>
+            {children}
+        </RateContext.Provider>
+    )
+}
